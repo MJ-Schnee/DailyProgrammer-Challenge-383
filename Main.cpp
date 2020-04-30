76fb0cf69bc242ab278f75ff796e7e96f73fbb4b
MJ <MJaSchnee@gmail.com>
2020-04-30 18:26:05 -0400

    Finished bonus 1

--- Main.cpp
+++ Main.cpp
@@ -18,8 +18,29 @@ bool compareNecklaces(std::string input1, std::string input2) {
     return false;
 }
 
+int repeats(std::string input) {
+    // Create a copy of the original input
+    std::string originalInput{ input };
+
+    int numRepeats{ 0 };
+
+    // Loop through for the number of letters
+    for (std::string::size_type i = 0; i < input.length(); i++) {
+        // Take the last letter of the input and append the substring up till then to it
+        input = input[input.length() - 1] + input.substr(0, input.length() - 1);
+
+        if (input.compare(originalInput) == 0)
+            numRepeats++;
+    }
+
+    return numRepeats;
+}
+
 int main() {
     std::string input1{ "" }, input2{ "" };
+
+    // Comparing two necklaces
+    /*
 
     std::cout << "Please enter the first necklace: ";
     std::cin >> input1;
@@ -31,4 +52,13 @@ int main() {
         std::cout << "These are the same necklace\n";
     else
         std::cout << "These are NOT the same necklace\n";
+    */
+
+    // Checking for repeats
+    std::cout << "What does the bead necklace say? ";
+    std::cin >> input1;
+
+    std::cout << "That repeats " << repeats(input1) << " time(s)\n";
+
+    return 0;
 }
